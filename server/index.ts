0c37de8e9ae907a18389e35f8f68c8932e878542
sewalu steven <sewalusteven@gmail.com>
2023-10-10 10:20:19 +0300

    keycloak changes

--- server/index.ts
+++ server/index.ts
@@ -1,28 +1,35 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import session from 'express-session';
-import Keycloak from 'keycloak-connect';
+import { LocalStorage } from "node-localstorage";
 
 import sample from "./api/sample";
 const app = express()
+const localStorage = new LocalStorage('./storage');
 
-
-const memoryStore = new session.MemoryStore();
-app.use(session({
-    secret:'someSecret',
-    resave: false,
-    saveUninitialized: true,
-    store: memoryStore
-}))
-
-
-const keycloak = new Keycloak({ store: memoryStore });
 app.use(bodyParser.json());
 app.use(cors());
-app.use( keycloak.middleware());
 
-app.use('/api/v1', sample);
+app.use('/api/v1/add-token',(req, res) => {
+    let {token} =  req.body
+    localStorage.setItem('access-token', `Bearer ${token}`);
+});
+
+const checkTokenMiddleware =  (req:Request, res:Response, next:NextFunction) => {
+   if(localStorage.getItem('access-token') !== null){
+       let token = localStorage.getItem('access-token');
+       if(token !== req.headers.authorization){
+           res.status(401).json({error: 'Unauthorized'})
+       }
+       next()
+   }
+    res.status(401).json({error: 'Unauthorized'})
+}
+
+app.use(checkTokenMiddleware);
+
+
+app.use('/api/v1',  sample);
 
 const server = app.listen(3000, () =>
   console.log(`
